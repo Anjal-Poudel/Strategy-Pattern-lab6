47c91da7c98cb89fee6f58902ca24b067e1ccb90
anjal <anjalpoudel@gmail.com>
2025-03-09 21:42:52 +0530

    Update the Duck class

--- Duck.java
+++ Duck.java
@@ -1,5 +1,31 @@
-package lab-java.assignment_6;
+public abstract class Duck {
+    QuackBehaviour quackBehaviour;
+    FlyBehavior flyBehaviour;
+    SwimBehaviour swimBehaviour;
 
-public class Duck {
-    
+    public void performQuack() {
+        quackBehaviour.quack();
+    }
+
+    public void performFly() {
+        flyBehaviour.fly();
+    }
+
+    public void performSwim() {
+        swimBehaviour.swim();
+    }
+
+    public void setQuackBehaviour(QuackBehaviour qb) {
+        quackBehaviour = qb;
+    }
+
+    public void setFlyBehaviour(FlyBehavior fb) {
+        flyBehaviour = fb;
+    }
+
+    public void setSwimBehaviour(SwimBehaviour sb) {
+        swimBehaviour = sb;
+    }
+
+    public abstract void display();
 }
